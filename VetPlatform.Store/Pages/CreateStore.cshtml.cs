11d999d6cdf49658432573b3286bccbbbdf4796a
GzimH <gzim.helshanii@gmail.com>
2019-07-20 23:08:50 +0200

    wip: Create your own store form

---
+++ VetPlatform.Store/Pages/CreateStore.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VetPlatform.Store.Models;
+
+namespace VetPlatform.Store.Pages
+{
+    public class CreateStoreModel : PageModel
+    {
+        [BindProperty]
+        public CreateStoreRequestModel Store { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
